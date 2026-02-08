9e4add6a70913ed93f06af87b6c5036cf14a29aa
Les Aubrey <les@wharf.digital>
2026-02-08 17:49:57 +0800

    Wire up sanity package to next.js app

--- apps/web/src/app/layout.tsx
+++ apps/web/src/app/layout.tsx
@@ -3,6 +3,8 @@ import { Geist, Geist_Mono } from "next/font/google";
 
 import "@workspace/ui/globals.css";
 
+import { SanityLive } from "@workspace/sanity/live";
+
 const fontSans = Geist({
   subsets: ["latin"],
   variable: "--font-sans",
@@ -29,6 +31,7 @@ export default function RootLayout({
         className={`${fontSans.variable} ${fontMono.variable} font-sans antialiased`}
       >
         {children}
+        <SanityLive />
       </body>
     </html>
   );
