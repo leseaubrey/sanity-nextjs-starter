224a2545f60ae2036ba1da5aa32b075f10fd3156
Les Aubrey <les@wharf.digital>
2026-02-09 16:09:35 +0800

    Add initial post document

---
+++ packages/sanity/src/schemas/documents/post.ts
@@ -0,0 +1,36 @@
+import { defineField, defineType } from "sanity";
+
+export const postType = defineType({
+  name: "post",
+  title: "Post",
+  type: "document",
+  groups: [{ name: "content", title: "Content", default: true }],
+  fields: [
+    defineField({
+      name: "title",
+      title: "Title",
+      type: "string",
+      group: "content",
+      validation: (rule) => rule.required(),
+    }),
+    defineField({
+      name: "slug",
+      title: "Slug",
+      type: "slug",
+      group: "content",
+      options: {
+        source: "title",
+        maxLength: 96,
+      },
+      validation: (rule) => rule.required(),
+    }),
+  ],
+  preview: {
+    select: {
+      title: "title",
+    },
+    prepare: ({ title }) => ({
+      title: `${title ?? "Untitled Post"}`,
+    }),
+  },
+});
