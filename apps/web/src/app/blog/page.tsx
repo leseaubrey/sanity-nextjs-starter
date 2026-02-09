9cc7bb45e5c901e15fb71a4ee2c537d3b3e60d01
Les Aubrey <les@wharf.digital>
2026-02-09 17:06:42 +0800

    Add initial blog and post templates

---
+++ apps/web/src/app/blog/page.tsx
@@ -0,0 +1,18 @@
+import { fetchPageBySlug } from "@workspace/sanity/queries";
+
+export default async function BlogPage() {
+  // TODO: Magic string
+  const result = await fetchPageBySlug("blog");
+
+  if (!result.data) {
+    return <div>Page not found</div>;
+  }
+
+  return (
+    <div className="flex min-h-svh items-center justify-center">
+      <div className="flex flex-col items-center justify-center gap-4">
+        <h1 className="text-2xl font-bold">{result.data.title}</h1>
+      </div>
+    </div>
+  );
+}
