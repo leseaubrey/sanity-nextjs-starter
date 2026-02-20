9eda132d38566e29e72e5108bbe07774e92dfba3
Les Aubrey <les@wharf.digital>
2026-02-20 14:54:25 +0800

    Clean up

--- apps/web/src/app/page.tsx
+++ apps/web/src/app/page.tsx
@@ -13,11 +13,17 @@ export default async function HomePage() {
   return (
     <div className="container mx-auto py-16">
       <Link href="/blog">Blog</Link>
+      <br />
       <Link href="/events">Events</Link>
+      <br />
       <Link href="/people">People</Link>
+      <br />
       <Link href="/projects">Projects</Link>
+      <br />
       <Link href="/publications">Publications</Link>
+      <br />
       <Link href="/regions">Regions</Link>
+      <br />
       <Link href="/themes">Themes</Link>
     </div>
   );
